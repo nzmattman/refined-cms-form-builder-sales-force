bd3fa8abd7eaa005d49fa80cf74efbfb70de12a2
Matthias Mac <matthias-mac@refineddigital.co.nz>
2020-07-23 07:55:35 +0000

    initial commit

---
+++ src/SalesForce/Module/Providers/FormBuilderSalesForceServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RefinedDigital\FormBuilder\SalesForce\Module\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormBuilderSalesForceServiceProvider extends ServiceProvider
+{
+}
